c41857440b4d8815c5329eefab438d733afeae3a
alan.invents <alan.invents@gmail.com>
2017-12-01 08:44:37 -0500

    Create example.js

---
+++ Property-js/example.js
@@ -0,0 +1,14 @@
+//Make sure the code for Property.js is at the top of you project file or in the <head> tag.
+/** In the <body> tag:
+ * 
+ * <div id="div-1">div</div>
+ * 
+**/
+
+var div1 = new object({
+	el: "div-1"
+	
+	
+	
+	
+});
