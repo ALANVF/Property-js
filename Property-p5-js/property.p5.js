49468cae5fe5153246205ff4755772a4134b714d
alan.invents <alan.invents@gmail.com>
2018-02-01 12:20:03 -0500

    Added some new reference

--- Property-p5-js/property.p5.js
+++ Property-p5-js/property.p5.js
@@ -1,3 +1,8 @@
+/*
+ * Most things is the p5 version are the same as the normal version. Anything different will be described.
+*/
+
+//col instaed of color because p5 has a function called color(and it seems broken)
 function col(red, green, blue) {
 	if(green === undefined && blue === undefined) {
 		return {
@@ -57,7 +62,7 @@ function object(p) {
 	    this.f = p.f || col(255, 255, 255);
 	    this.s = p.s || col(0, 0, 0);
 	    this.c = p.c || col(0, 0, 0);
-	    this.txt = p.txt || "";
+	    //this.txt = p.txt || "";
 	    this.t = p.t || 1;
 	}else{
 		this.el = document.getElementById(p.el);
@@ -65,12 +70,11 @@ function object(p) {
 		this.x = p.x || parseInt(this.el.style.left);
 		this.y = p.y || parseInt(this.el.style.top);
 		this.w = p.w || parseInt(this.el.style.width);
-		//this.w = p.w || this.el.style.width;
 		this.h = p.h || parseInt(this.el.style.height);
 		this.f = p.f || rgbToColor(this.el.style.backgroundColor);
 		this.s = p.s || rgbToColor(this.el.style.borderColor) || "transparent" || col(255);
 		this.c = p.c || col(0) || rgbToColor(this.el.style.color);
-		this.txt = p.txt || this.el.textContent;
+		//this.txt = p.txt || this.el.textContent;
 		this.t = p.t || this.el.style.borderWeight || 1;
 	}
 }
@@ -85,6 +89,26 @@ object.prototype.create = function(asDOMElement) {
 		console.log("error");
 	}
 };
+
+/*
+ * @method collide
+ * @param {object} [p.o] The object to check collision for
+ * @param {Array[speedX, speedY]} [movingSpeed] The speed at which the current object is moving. Optional
+ * @example
+ * p1.collide({
+ *     o: p2,
+ *     s: [5, 0]
+ * }); //p1 will only collide with p2 if it hits the left or right at a speed of 5
+ * 
+ * p1.collide({
+ *     o: p3
+ * }); //p1 will always collide with p3 on all sides at any speed
+ * 
+ * if(p1.collide({o: p4})) {
+ *     p4.f = col(0);
+ * } //when p1 collides with p4, p4's fill color will be black
+ * @return colliding
+*/
 object.prototype.collide = function(p) {
 	if(p.s === undefined) {
 		p.s = [dist(this.x + this.w/2, 0, p.o.x + p.o.w/2, 0) - (this.w/2 + p.o.w/2), dist(0, this.y + this.h/2, 0, p.o.y + p.o.h/2) - (this.h/2 + p.o.h/2)];
@@ -108,6 +132,8 @@ object.prototype.collide = function(p) {
 		}else{return false;}
 	}
 };
+
+//If left blank, it returns true or false depending on if the object is clicked on or not
 object.prototype.button = function(p) {
 	if(p !== undefined) {
 		if(this.el === undefined) {
@@ -165,7 +191,7 @@ object.prototype.mouse = function(p) {
 			default:
 			    break;
 		}
-	}else{
+	}/*else{
 		switch(p.e) {
 			case "click":
 				if(mouseIsPressed && dist(mouseX, mouseY, this.x + this.r/2, this.y + this.r/2) <= this.r/2 && dist(mouseX, mouseY, pmouseX, pmouseY) <= 1) {
@@ -191,7 +217,7 @@ object.prototype.mouse = function(p) {
 			default:
 			    break;
 		}
-	}
+	}*/
 };
 object.prototype.move = function(p) {
 	if(this.x < p.t[0]) {
@@ -205,6 +231,8 @@ object.prototype.move = function(p) {
 		this.y -= (this.y <= p.t[1]) ? 0 : p.s[1];
 	}
 };
+
+//now returns the changed object
 object.prototype.inherit = function(p) {
 	this.x = (p.v[0] == 1) ? p.o.x : (p.v[0] == 2) ? this.x + p.o.x : this.x;
 	this.y = (p.v[1] == 1) ? p.o.y : (p.v[1] == 2) ? this.y + p.o.y : this.y;
@@ -213,4 +241,5 @@ object.prototype.inherit = function(p) {
 	this.f = (p.v[4] == 1) ? p.o.f : this.f;
 	this.s = (p.v[5] == 1) ? p.o.s : this.s;
 	this.t = (p.v[6] == 1) ? p.o.t : (p.v[6] == 2) ? this.t + p.o.t : this.t;
+	return this;
 };
